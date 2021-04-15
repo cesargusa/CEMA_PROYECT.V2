ea9f615126c3e421ed3412e73be43b77dc9069e0
Mario <gutierrezmontoriomario@gmail.com>
2021-04-15 10:39:21 +0200

    clase de producto view controller

---
+++ src/main/java/proyect/cema/Web/VIew/ProductViewController.java
@@ -0,0 +1,28 @@
+package proyect.cema.Web.VIew;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import proyect.cema.Services.Models.ProductDTO;
+import proyect.cema.Web.API.ProductController;
+
+
+@Controller
+public class ProductViewController {
+    private final ProductController productController;
+
+    ProductViewController(ProductController productController){
+        this.productController=productController;
+    }
+
+    @GetMapping("/productos")
+    public ModelAndView GetAllProducts(){
+        List<ProductDTO> allProducts = productController.GetAll();
+        ModelAndView mv = new ModelAndView("productos");
+        mv.addObject("productos", allProducts);
+        return mv;
+    }
+}

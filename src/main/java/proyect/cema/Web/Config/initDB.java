c0cda357548e8ae91b462154e43c806a2f2c8c3b
Mario <gutierrezmontoriomario@gmail.com>
2021-04-15 11:33:17 +0200

    Creamos el productos.html donde esta el control de los productos con todas sus variables

--- src/main/java/proyect/cema/Web/Config/initDB.java
+++ src/main/java/proyect/cema/Web/Config/initDB.java
@@ -20,10 +20,10 @@ public class initDB {
             usuario.save(new UsuarioEntity("marieteloquete@gmail.comerevrerveevv","123","Marirevervreo","Gutierrezveveerv","https://parma.repubblica.it/images/2012/09/14/141819337-d98f658e-289c-4a0d-8f00-20d8c5063773.jpg","123445654veevervrv5","Madridvrevrrvre","Españavrervrrevr","reuh9eruheuhvuherhvr0uhrvrhvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvverferv"));
 
 
-            product.save(new ProductEntity("Sudadera Nike",40L,"L","https://images.asos-media.com/products/sudadera-azul-marino-con-capucha-club-de-nike-plus/12495764-1-navy?$XXL$&wid=513&fit=constrain","azul marino","Como nuevo","Nike","Hombre"));
-            product.save(new ProductEntity("Pantalon Stradivarius",23L,"43","https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/elle-pantalon-negro-barato-stradivarius-c-1559142955.jpg?resize=480:*","negro","Sin estrenar","Stradivarius","Hombre"));
-            product.save(new ProductEntity("Camiseta Snipes",15L,"M","https://www.snipes.es/dw/image/v2/BDCB_PRD/on/demandware.static/-/Sites-snse-master-eu/default/dwf079653a/1964803_P.jpg?sw=780&sh=780&sm=fit&sfrm=png","rojo","En buen estado ","Snipes","Mujer"));
-            product.save(new ProductEntity("Cazadora North Face",170L,"XL","https://http2.mlstatic.com/casaca-chaqueta-mujer-north-face-fibra-800-cintura-D_NQ_NP_800779-MPE27648121921_062018-F.jpg","blanca","Como nuevo","North face","Hombre"));
+            product.save(new ProductEntity("Sudadera Nike",40L,"L","https://images.asos-media.com/products/sudadera-azul-marino-con-capucha-club-de-nike-plus/12495764-1-navy?$XXL$&wid=513&fit=constrain","Azul Marino","Como nuevo","Nike","Hombre"));
+            product.save(new ProductEntity("Pantalon Stradivarius",23L,"43","https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/elle-pantalon-negro-barato-stradivarius-c-1559142955.jpg?resize=480:*","Negro","Sin estrenar","Stradivarius","Hombre"));
+            product.save(new ProductEntity("Camiseta Snipes",15L,"M","https://www.snipes.es/dw/image/v2/BDCB_PRD/on/demandware.static/-/Sites-snse-master-eu/default/dwf079653a/1964803_P.jpg?sw=780&sh=780&sm=fit&sfrm=png","Rojo","En buen estado ","Snipes","Mujer"));
+            product.save(new ProductEntity("Cazadora North Face",170L,"XL","https://http2.mlstatic.com/casaca-chaqueta-mujer-north-face-fibra-800-cintura-D_NQ_NP_800779-MPE27648121921_062018-F.jpg","blanca","Como nuevo","North Face","Hombre"));
             product.save(new ProductEntity("Cortavientos Adidas ",50L,"M","https://images-na.ssl-images-amazon.com/images/I/713Y%2BqQdKmL._AC_UY879_.jpg","verde","Sin estrenar","Adidas","Mujer"));
       
         };

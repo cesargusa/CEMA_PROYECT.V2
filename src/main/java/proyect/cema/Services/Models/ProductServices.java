238565c958589e997c7f7225ff0d703ac99d9c2a
Mario <gutierrezmontoriomario@gmail.com>
2021-04-19 09:00:04 +0200

    He añadido el getById en la clases Product Controller y en el Product Service

--- src/main/java/proyect/cema/Services/Models/ProductServices.java
+++ src/main/java/proyect/cema/Services/Models/ProductServices.java
@@ -1,12 +1,15 @@
 package proyect.cema.Services.Models;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import ch.qos.logback.core.joran.conditional.ElseAction;
+import proyect.cema.Repositories.Entities.ProductEntity;
 import proyect.cema.Repositories.Entities.UsuarioEntity;
 import proyect.cema.Repositories.Interfaces.ProductRepository;
 import proyect.cema.Repositories.Interfaces.UsuarioRepository;
@@ -20,6 +23,38 @@ public class ProductServices {
         return productRepository.findAll().stream()
         .map(x -> modelMapper.map(x, ProductDTO.class))
         .collect(Collectors.toList());
+    }
+
+    public ProductDTO add(ProductDTO product){
+        ProductEntity entitytoInsert = modelMapper.map(product, ProductEntity.class);
+        ProductEntity result = productRepository.save(entitytoInsert);
+        return modelMapper.map(result, ProductDTO.class);
+
+    }
+
+    public ProductDTO update(Long ID, ProductDTO product) {
+        ProductEntity entitytoUpdate = modelMapper.map(product, ProductEntity.class);
+        ProductEntity result = productRepository.save(entitytoUpdate);
+        return modelMapper.map(result, ProductDTO.class);
+    }
+
+    public void delete(Long ID){
+        Optional<ProductEntity> entitytoDelete = productRepository.findById(ID);
+        if(entitytoDelete.isPresent())
+        productRepository.delete(entitytoDelete.get());
+    }
+
+    public Optional<ProductDTO> findById(Long id){
+        Optional<ProductEntity> entity = productRepository.findById(id);
+        if(entity.isPresent())
+        return Optional.of(modelMapper.map(entity.get(), ProductDTO.class));
+        else
+        return Optional.empty();
+    }
+
+    public List<ProductDTO> getById(Long id){
+        return productRepository.findById(id).stream()
+        .map(x -> modelMapper.map(x,ProductDTO.class)).collect(Collectors.toList());WS
     }
 
 }

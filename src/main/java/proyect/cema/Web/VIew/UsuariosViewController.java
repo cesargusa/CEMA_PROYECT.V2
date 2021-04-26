1ba99dedfcc3528cdc77ff68597145414b79d00d
cesar gutiérrez <cesargusa-97@hotmail.com>
2021-04-26 10:56:26 +0200

    Añadido el buscar por id en usuarios y el boton de borrar los filtros

--- src/main/java/proyect/cema/Web/VIew/UsuariosViewController.java
+++ src/main/java/proyect/cema/Web/VIew/UsuariosViewController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import proyect.cema.Services.Models.UsuarioDTO;
@@ -20,6 +21,13 @@ public class UsuariosViewController {
     @GetMapping("/usuariosControl")
     public ModelAndView GetAllUsuariosControl() {
         List<UsuarioDTO> allUsuarios = usuarioController.GetAll();
+        ModelAndView mv = new ModelAndView("usuariosControl");
+        mv.addObject("usuarios", allUsuarios);
+        return mv;
+    }
+    @GetMapping("/usuariosControl/{id}")
+    public ModelAndView GetUsuariosControlId(@PathVariable("id") Long id) {
+        List<UsuarioDTO> allUsuarios = usuarioController.GetById(id);
         ModelAndView mv = new ModelAndView("usuariosControl");
         mv.addObject("usuarios", allUsuarios);
         return mv;
